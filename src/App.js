69113a0f03fbcd3cf2b9b6826bc2864f6a47a215
Oakley Windiate <oakley.windiate@gmail.com>
2022-05-12 14:18:24 -0600

    Delete boiler plate React code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+// import Ideas from './Ideas';
+// import Form from './Form';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
+    <main className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="title">Rancid Tomatillos</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
-    </div>
+    </main>
   );
 }
 
