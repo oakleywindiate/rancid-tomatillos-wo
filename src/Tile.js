a3c58494500d73f6844cd32af0cffa7a4270af3e
Oakley Windiate <oakley.windiate@gmail.com>
2022-05-12 15:39:06 -0600

    Create tile JSX

--- src/Tile.js
+++ src/Tile.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './Tile.css';
 
-const Tile = ({ title }) => {
+const Tile = ({ title, posterPath, averageRating, releaseDate }) => {
   return (
     <div className='tile'>
       <h3>{title}</h3>
+      <img src={posterPath} />
+      <p>Average Rating: {averageRating}</p>
+      <p>Release Date: {releaseDate}</p>
     </div>
   )
 }
