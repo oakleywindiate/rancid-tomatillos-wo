ed03340300a13b986dd76e294c13710dc06993a2
Oakley Windiate <oakley.windiate@gmail.com>
2022-05-14 13:39:14 -0600

    Update DetailTile to display movie

--- src/DetailTile.js
+++ src/DetailTile.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import './DetailTile.css';
 
-const DetailTile = ({ title, posterPath, averageRating, releaseDate, id, seeAllMovies }) => {
+const DetailTile = (props) => {
   return (
     <div className='detail-tile'>
-      <img src={posterPath} />
-      <h3>{title}</h3>
-      <p>Average Rating: {averageRating.toFixed(2)}</p>
-      <p>Release Date: {releaseDate}</p>
-      <button className="go-back" onClick={() => seeAllMovies()}>Go Back to All Movies</button>
+      <img src={props.movieDetails.poster_path} />
+      <h3>{props.movieDetails.title}</h3>
+      <p>Average Rating: {props.movieDetails.average_rating}</p>
+      <p>Release Date: {props.movieDetails.release_date}</p>
+      <p>{props.movieDetails.overview}</p>
+      <p>Genres:{props.movieDetails.genres}</p>
+      <p>Budget: ${props.movieDetails.budget}</p>
+      <p>Revenue: ${props.movieDetails.revenue}</p>
+      <p>Runtime: {props.movieDetails.runtime} minutes</p>
+      <p>{props.movieDetails.tagline}</p>
+      <button className="go-back" onClick={() => props.movieDetails.seeAllMovies()}>Go Back to All Movies</button>
     </div>
   )
 }
