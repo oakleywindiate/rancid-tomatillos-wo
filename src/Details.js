9f39a3f8e09a0a80bca8b732493a39c9d4137781
Oakley Windiate <oakley.windiate@gmail.com>
2022-05-14 13:39:33 -0600

    Remove functionality from Details.js

--- src/Details.js
+++ src/Details.js
@@ -2,16 +2,9 @@ import React, { Component } from 'react';
 import DetailTile from './DetailTile'
 import './Details.css';
 
-const Details = ( {movieDetails, seeAllMovies} ) => {
+const Details = (props) => {
   return (
     <DetailTile
-    id={movieDetails.id}
-    key={movieDetails.id}
-    title={movieDetails.title}
-    posterPath={movieDetails.poster_path}
-    averageRating={movieDetails.average_rating}
-    releaseDate={movieDetails.release_date}
-    seeAllMovies={seeAllMovies}
     />
   )
 }
