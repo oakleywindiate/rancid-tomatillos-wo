078f24207adc26992829bd76e1c32dea9364e4db
Whitney Perricone <whitney.perricone@gmail.com>
2022-05-18 11:57:50 -0600

    Start Router Refactor

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import DetailTile from './DetailTile';
 import Details from './Details'
 import './App.css';
 import { fetchAllMovies, fetchMovieDetails } from './apiCalls'
+import { Route } from 'react-router-dom';
+
 
 
 class App extends Component {
@@ -38,14 +40,23 @@ class App extends Component {
       <main className="App">
         <header className="App-header">
           <h1 className="title">Rancid Tomatillos</h1>
-          <p>
-          </p>
-          {!this.state.movieDetails && <Movies movieData={this.state.movieData} seeMovieDetails={this.seeMovieDetails} />}
+
+          // {!this.state.movieDetails && <Movies movieData={this.state.movieData} seeMovieDetails={this.seeMovieDetails} />}
           {this.state.movieDetails && <DetailTile
              movieDetails={this.state.movieDetails} seeAllMovies={this.seeAllMovies} />}
           {this.state.error.length > 0 && <h2 className="error">{this.state.error}</h2>}
-        </header>
-      </main>
+
+          <Route exact path='/' render={ () => <Movies movieData={this.state.movieData} seeMovieDetails={this.seeMovieDetails} /> }/>
+
+          <Route exact path="/movies/:id"
+          render={({ match }) => { const movieToRender = this.state.movieData.find(movie => movie.id === parseInt(match.params.id));
+            console.log(match.params.id);
+            return <movieDetails {...movieToRender} />
+          }}
+          />
+          </header>
+
+          </main>
     );
   }
 }
