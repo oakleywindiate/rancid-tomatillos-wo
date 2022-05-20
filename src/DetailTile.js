f2b4c32f41e9d7cbca707947527c7ba3b7eb4c97
Whitney Perricone <whitney.perricone@gmail.com>
2022-05-20 11:31:27 -0600

    Update DetailTile

--- src/DetailTile.js
+++ src/DetailTile.js
@@ -49,22 +49,6 @@ class DetailTile extends Component {
   }
 }
 
-// const DetailTile = (props) => {
-//   return (
-//     <div className='detail-tile'>
-//       <img src={props.movieDetails.poster_path} />
-//       <h3>{props.movieDetails.title}</h3>
-//       <p>Average Rating: {props.movieDetails.average_rating}</p>
-//       <p>Release Date: {props.movieDetails.release_date}</p>
-//       <p>{props.movieDetails.overview}</p>
-//       <p>Genres:{props.movieDetails.genres}</p>
-//       <p>Budget: ${props.movieDetails.budget}</p>
-//       <p>Revenue: ${props.movieDetails.revenue}</p>
-//       <p>Runtime: {props.movieDetails.runtime} minutes</p>
-//       <p>{props.movieDetails.tagline}</p>
-//       <button className="go-back" onClick={() => props.seeAllMovies()}>Go Back to All Movies</button>
-//     </div>
-//   )
-// }
+
 
 export default DetailTile;
